c1e35f54163dfb22bcf2bb798ddc286731ffc0cd
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-08-07 23:54:58 +0600

    rough report generated successfully

--- top_perchaser/routes/web.php
+++ top_perchaser/routes/web.php
@@ -20,55 +20,75 @@ Route::get('/', function () {
 });
 
 Route::get('/report', function () {
-    $report = Http::get('https://raw.githubusercontent.com/Bit-Code-Technologies/mockapi/main/purchase.json');
+    // $report = Http::get('https://raw.githubusercontent.com/Bit-Code-Technologies/mockapi/main/purchase.json');
 
-    $twoDarr = json_decode($report, true);
+    // $twoDarr = json_decode($report, true);
+    //////    Fetched Data     //////////
 
     // $singleArray = array();
     // foreach ($report as $key => $value) {
     //     $singleArray[$key] = $value['plan'];
     // }
 
-    dump(count($twoDarr));
+    /////////     UNCOMMENT THIS PART     ////////
+    // dump(count($twoDarr));
 
-    $arr_count = count($twoDarr);
+    // $arr_count = count($twoDarr);
 
-    for ($i = 0; $i < $arr_count; $i++) {
-        $singleArray = $twoDarr[$i];
-        $singleObject = (object)$singleArray;
-        DB::table('report')->insert([
-            'name' => $singleObject->name,
-            'product_name' => $singleObject->product_name,
-            'product_price' => $singleObject->product_price,
-            'purchase_quantity' => $singleObject->purchase_quantity,
-        ]);
-        dump($singleObject);
-        // $singleArray = array_reduce($twoDarr, 'array_merge', array());
-        // dump($singleArray);
+    // for ($i = 0; $i < $arr_count; $i++) {
+    //     $singleArray = $twoDarr[$i];
+    //     $singleObject = (object)$singleArray;
+    //     DB::table('report')->insert([
+    //         'name' => $singleObject->name,
+    //         'product_name' => $singleObject->product_name,
+    //         'product_price' => $singleObject->product_price,
+    //         'purchase_quantity' => $singleObject->purchase_quantity,
+    //     ]);
+    //     dump($singleObject);
+    //     // $singleArray = array_reduce($twoDarr, 'array_merge', array());
+    //     // dump($singleArray);
 
-        // for ($j = 0; $j < count($singleArray); $i++) {
-        //     $singleArray = array_reduce($twoDarr, 'array_merge', array());
-        //     dump($singleArray);
-        // }
-    }
+    //     // for ($j = 0; $j < count($singleArray); $i++) {
+    //     //     $singleArray = array_reduce($twoDarr, 'array_merge', array());
+    //     //     dump($singleArray);
+    //     // }
+    // }
 
-    dd();
+    /////////     UNCOMMENT THIS PART     ////////
 
-    $first_obj = (object)$report[0];
 
-    DB::table('report')->insert([
-        'name' => $first_obj->name,
-        'product_name' => $first_obj->product_name,
-        'product_price' => $first_obj->product_price,
-        'purchase_quantity' => $first_obj->purchase_quantity,
-    ]);
+    // $leaderBoard = DB::table('incomes')
+    //     ->where('income_type', 'Referral')
+    //     ->orWhere('income_type', 'Generation')
+    //     ->select('user_name', DB::raw('SUM(amount) as total_income'))
+    //     ->groupBy('user_name')
+    //     ->orderBy('total_income', 'desc')
+    //     ->take(20)
+    //     ->get();
 
-    dump($first_obj);
-    dump($first_obj->created_at);
-    dump($report[0]['name']);
-    dd('json:', $report->json());
-    // dump(count($report));
-    // return $report->json();
-    dump($report[0]->name);
-    return json_decode($report);
+    $top_purchaser = DB::table('report')
+        ->select('name', DB::raw('SUM(product_price*purchase_quantity) as total_amount'))
+        ->groupBy('name')
+        ->orderBy('total_amount', 'desc')
+        ->get();
+
+    dd($top_purchaser);
+
+    // $first_obj = (object)$report[0];
+
+    // DB::table('report')->insert([
+    //     'name' => $first_obj->name,
+    //     'product_name' => $first_obj->product_name,
+    //     'product_price' => $first_obj->product_price,
+    //     'purchase_quantity' => $first_obj->purchase_quantity,
+    // ]);
+
+    // dump($first_obj);
+    // dump($first_obj->created_at);
+    // dump($report[0]['name']);
+    // dd('json:', $report->json());
+    // // dump(count($report));
+    // // return $report->json();
+    // dump($report[0]->name);
+    // return json_decode($report);
 });
