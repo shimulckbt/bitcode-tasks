df1e9df6737bc3d39d00770bbaab9849d8c7f41b
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-08-07 18:13:57 +0600

    fetched data successfully

--- top_perchaser/routes/web.php
+++ top_perchaser/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,58 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/report', function () {
+    $report = Http::get('https://raw.githubusercontent.com/Bit-Code-Technologies/mockapi/main/purchase.json');
+
+    $twoDarr = json_decode($report, true);
+
+    // $singleArray = array();
+    // foreach ($report as $key => $value) {
+    //     $singleArray[$key] = $value['plan'];
+    // }
+
+    dump(count($twoDarr));
+
+    $arr_count = count($twoDarr);
+
+    for ($i = 0; $i < $arr_count; $i++) {
+        $singleArray = $twoDarr[$i];
+        $singleObject = (object)$singleArray;
+        DB::table('report')->insert([
+            'name' => $singleObject->name,
+            'product_name' => $singleObject->product_name,
+            'product_price' => $singleObject->product_price,
+            'purchase_quantity' => $singleObject->purchase_quantity,
+        ]);
+        dump($singleObject);
+        // $singleArray = array_reduce($twoDarr, 'array_merge', array());
+        // dump($singleArray);
+
+        // for ($j = 0; $j < count($singleArray); $i++) {
+        //     $singleArray = array_reduce($twoDarr, 'array_merge', array());
+        //     dump($singleArray);
+        // }
+    }
+
+    dd();
+
+    $first_obj = (object)$report[0];
+
+    DB::table('report')->insert([
+        'name' => $first_obj->name,
+        'product_name' => $first_obj->product_name,
+        'product_price' => $first_obj->product_price,
+        'purchase_quantity' => $first_obj->purchase_quantity,
+    ]);
+
+    dump($first_obj);
+    dump($first_obj->created_at);
+    dump($report[0]['name']);
+    dd('json:', $report->json());
+    // dump(count($report));
+    // return $report->json();
+    dump($report[0]->name);
+    return json_decode($report);
 });
