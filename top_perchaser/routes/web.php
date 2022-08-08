a8fac14895280731a5e8eef014491cc328572657
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-08-08 22:51:31 +0600

    filtering duplicate record

--- top_perchaser/routes/web.php
+++ top_perchaser/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\Console\Input\Input;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,9 +21,11 @@ Route::get('/', function () {
 });
 
 Route::get('/report', function () {
-    // $report = Http::get('https://raw.githubusercontent.com/Bit-Code-Technologies/mockapi/main/purchase.json');
+    $report = Http::get('https://raw.githubusercontent.com/Bit-Code-Technologies/mockapi/main/purchase.json');
 
-    // $twoDarr = json_decode($report, true);
+
+    $twoDarrOld = json_decode($report, true);
+
     //////    Fetched Data     //////////
 
     // $singleArray = array();
@@ -31,14 +34,73 @@ Route::get('/report', function () {
     // }
 
     /////////     UNCOMMENT THIS PART     ////////
-    // dump(count($twoDarr));
 
-    // $arr_count = count($twoDarr);
+    $twoDarrNew = array(
+        12 => array(
+            "created_at" => "2022-08-02T06:35:16.704Z",
+            "name" => "Shimul",
+            "order_no" => 31383123,
+            "user_phone" => "288-595-1866 s687",
+            "product_code" => "f472e9e7-a197-4c6b-bafe-bed21ff5d707",
+            "product_name" => "Chair",
+            "product_price" => "860.00",
+            "purchase_quantity" => 2,
+        ),
+        13 => array(
+            "created_at" => "2022-08-02T06:35:16.704Z",
+            "name" => "Palash",
+            "order_no" => 313831234,
+            "user_phone" => "288-595-1866 p687",
+            "product_code" => "f472e9e7-a197-4c6b-bafe-bed21ff5d707",
+            "product_name" => "Chair",
+            "product_price" => "860.00",
+            "purchase_quantity" => 2,
+        ),
+    );
+
+    $twoDarr = array_merge($twoDarrOld, $twoDarrNew);
+
+    dump(count($twoDarr));
+    // dd($twoDarr);
+
+    $arr_count = count($twoDarr);
+    if (empty(DB::table('report')->count())) {
+        for ($i = 0; $i < $arr_count; $i++) {
+            $singleArray = $twoDarr[$i];
+            $singleObject = (object)$singleArray;
+            DB::table('report')->insert([
+                'order_no' => $singleObject->order_no,
+                'name' => $singleObject->name,
+                'product_name' => $singleObject->product_name,
+                'product_price' => $singleObject->product_price,
+                'purchase_quantity' => $singleObject->purchase_quantity,
+            ]);
+        }
+    } else {
+        for ($i = 0; $i < $arr_count; $i++) {
+            $singleArray = $twoDarr[$i];
+            $singleObject = (object)$singleArray;
+            $exists = DB::table('report')->where('order_no', $singleObject->order_no)->first();
+
+            if ($exists) {
+                continue;
+            } else {
+                DB::table('report')->insert([
+                    'order_no' => $singleObject->order_no,
+                    'name' => $singleObject->name,
+                    'product_name' => $singleObject->product_name,
+                    'product_price' => $singleObject->product_price,
+                    'purchase_quantity' => $singleObject->purchase_quantity,
+                ]);
+            }
+        }
+    }
 
     // for ($i = 0; $i < $arr_count; $i++) {
     //     $singleArray = $twoDarr[$i];
     //     $singleObject = (object)$singleArray;
     //     DB::table('report')->insert([
+    //         'order_no' => $singleObject->order_no,
     //         'name' => $singleObject->name,
     //         'product_name' => $singleObject->product_name,
     //         'product_price' => $singleObject->product_price,
@@ -57,15 +119,6 @@ Route::get('/report', function () {
     /////////     UNCOMMENT THIS PART     ////////
 
 
-    // $leaderBoard = DB::table('incomes')
-    //     ->where('income_type', 'Referral')
-    //     ->orWhere('income_type', 'Generation')
-    //     ->select('user_name', DB::raw('SUM(amount) as total_income'))
-    //     ->groupBy('user_name')
-    //     ->orderBy('total_income', 'desc')
-    //     ->take(20)
-    //     ->get();
-
     $top_purchaser = DB::table('report')
         ->select('name', DB::raw('SUM(product_price*purchase_quantity) as total_amount'))
         ->groupBy('name')
@@ -74,21 +127,5 @@ Route::get('/report', function () {
 
     dd($top_purchaser);
 
-    // $first_obj = (object)$report[0];
-
-    // DB::table('report')->insert([
-    //     'name' => $first_obj->name,
-    //     'product_name' => $first_obj->product_name,
-    //     'product_price' => $first_obj->product_price,
-    //     'purchase_quantity' => $first_obj->purchase_quantity,
-    // ]);
-
-    // dump($first_obj);
-    // dump($first_obj->created_at);
-    // dump($report[0]['name']);
-    // dd('json:', $report->json());
-    // // dump(count($report));
-    // // return $report->json();
-    // dump($report[0]->name);
     // return json_decode($report);
 });
